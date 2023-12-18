49958f990ad4f2b5d371dda61dae3a54aea5a6ef
Rohit Nandi <rohitnandibmp@gmail.com>
2023-12-18 15:12:21 +0530

    working fine except the progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
+import { useRef } from "react";
 import "./App.css";
-import Home from "./components/home";
+import video from "./assets/video.mp4";
+import useVideoPlayer from "./hooks/videoplayer";
+<link
+  href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+  rel="stylesheet"
+/>;
 
-function App() {
+const App = () => {
+  const videoElement = useRef(null);
+  const {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoProgress,
+    handleVideoSpeed,
+    toggleMute,
+  } = useVideoPlayer(videoElement);
   return (
-    <>
-      <Home />
-    </>
+    <div className="container">
+      <div className="video-wrapper">
+        <video
+          src={video}
+          ref={videoElement}
+          onTimeUpdate={handleOnTimeUpdate}
+        />
+        <div className="controls">
+          <div className="actions">
+            <button onClick={togglePlay}>
+              {!playerState.isPlaying ? (
+                <i className="bx bx-play"></i>
+              ) : (
+                <i className="bx bx-pause"></i>
+              )}
+            </button>
+          </div>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={playerState.progress}
+            onChange={(e) => handleVideoProgress(e)}
+          />
+          <select
+            className="velocity"
+            value={playerState.speed}
+            onChange={(e) => handleVideoSpeed(e)}
+          >
+            <option value="0.50">0.50x</option>
+            <option value="1">1x</option>
+            <option value="1.25">1.25x</option>
+            <option value="2">2x</option>
+          </select>
+          <button className="mute-btn" onClick={toggleMute}>
+            {!playerState.isMuted ? (
+              <i className="bx bxs-volume-full"></i>
+            ) : (
+              <i className="bx bxs-volume-mute"></i>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
--- src/hooks/videoplayer.ts
+++ src/hooks/videoplayer.ts
@@ -1,40 +1,83 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-const videoPlayer = (videoElement: any) => {
+const useVideoPlayer = (videoElement: React.RefObject<HTMLVideoElement>) => {
   const [playerState, setPlayerState] = useState({
     isPlaying: false,
     progress: 0,
     speed: 1,
     isMuted: false,
   });
+
   const togglePlay = () => {
     setPlayerState({
       ...playerState,
       isPlaying: !playerState.isPlaying,
     });
   };
+
   useEffect(() => {
     playerState.isPlaying
-      ? videoElement.current.play()
-      : videoElement.current.pause();
+      ? videoElement?.current?.play()
+      : videoElement?.current?.pause();
   }, [playerState.isPlaying, videoElement]);
+
   const handleOnTimeUpdate = () => {
     const progress =
-      (videoElement.current.currentTime / videoElement.current.duration) * 100;
+      ((videoElement?.current?.currentTime || 0) /
+        (videoElement?.current?.duration || 1)) *
+      100;
     setPlayerState({
       ...playerState,
       progress,
     });
   };
-  const handleVideoProgress = (event) => {
+
+  const handleVideoProgress = (event: React.ChangeEvent<HTMLInputElement>) => {
     const manualChange = Number(event.target.value);
-    videoElement.current.currentTime =
-      (videoElement.current.duration / 100) * manualChange;
+    if (videoElement?.current?.currentTime) {
+      videoElement.current.currentTime =
+        (videoElement.current.duration / 100) * manualChange;
+    }
     setPlayerState({
       ...playerState,
       progress: manualChange,
     });
   };
+
+  const handleVideoSpeed = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const speed = Number(event.target.value);
+    if (videoElement?.current?.playbackRate) {
+      videoElement.current.playbackRate = speed;
+    }
+    setPlayerState({
+      ...playerState,
+      speed,
+    });
+  };
+
+  const toggleMute = () => {
+    setPlayerState({
+      ...playerState,
+      isMuted: !playerState.isMuted,
+    });
+  };
+
+  useEffect(() => {
+    if (videoElement?.current?.muted) {
+      playerState.isMuted
+        ? (videoElement.current.muted = true)
+        : (videoElement.current.muted = false);
+    }
+  }, [playerState.isMuted, videoElement]);
+
+  return {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoProgress,
+    handleVideoSpeed,
+    toggleMute,
+  };
 };
 
-export default videoPlayer;
+export default useVideoPlayer;
